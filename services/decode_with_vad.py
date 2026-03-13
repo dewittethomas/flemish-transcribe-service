bae5043bef7a2d0343c9f37f17d5f1edcae7efa4
Thomas Dewitte <thomasdewittecontact@gmail.com>
2026-03-13 21:41:18 +0000

    Renamed variables in decode file

--- services/decode_with_vad.py
+++ services/decode_with_vad.py
@@ -1,6 +1,5 @@
-from services.utils.espnet_model import load_wav, load_nelf_model
-from services.utils.vad_model import load_vad, load_audio
-from services.utils.postprocessing import format_output, write_output_to_file, merge_batch_outputs
+from services.utils.vad_model import load_audio
+from services.utils.postprocessing import format_output, merge_batch_outputs
 import services.model_loader as model_loader
 import torch
 import time
@@ -34,45 +33,45 @@ def transcribe(audio: BinaryIO) -> dict:
     # apply VAD
     speech_timestamps = vad_model.get_speech_timestamps(
         audio_file,
-        min_speech_duration_ms=min_speech_duration_ms,
-        min_silence_duration_ms=min_silence_duration_ms,
-        speech_pad_ms=speech_pad_ms,
-        threshold=vad_threshold,
+        min_speech_duration_ms=settings.min_speech_duration_ms,
+        min_silence_duration_ms=settings.min_silence_duration_ms,
+        speech_pad_ms=settings.speech_pad_ms,
+        threshold=settings.vad_threshold,
     )
 
     # merge VAD segments
     speech_segments = vad_model.merge_segments(
         speech_timestamps,
-        max_pause,
-        max_segment_length,
-        min_segment_length,
+        settings.max_pause,
+        settings.max_segment_length,
+        settings.min_segment_length,
         mode="hierarchical",
     )
 
     # decode speech segments to text
-    if batch_decode:
+    if settings.batch_decode:
         # combine all segments and decode simultaneously
-        wav_batch, wav_lens = [], []
+        audio_batch, audio_lens = [], []
         for seg in speech_segments:
-            wav_seg = audio_file[int(seg['start']*SR):int(seg['end']*SR)].to(device=device)
-            wav_len = torch.tensor([wav_seg.shape[0],], dtype=torch.long, device=device)
-            wav_batch.append(wav_seg)
-            wav_lens.append(wav_len)
+            audio_seg = audio_file[int(seg['start']*SR):int(seg['end']*SR)].to(device=settings.device)
+            audio_len = torch.tensor([audio_seg.shape[0],], dtype=torch.long, device=settings.device)
+            audio_batch.append(audio_seg)
+            audio_lens.append(audio_len)
     
-        wav_batch = torch.nn.utils.rnn.pad_sequence(wav_batch, batch_first=True)
-        wav_lens = torch.cat(wav_lens)
-        res = model.decode(wav_batch, wav_lens, ctc=encoder_outputs, verbatim=verbatim_decoder_outputs, subtitle=subtitle_decoder_outputs)
+        audio_batch = torch.nn.utils.rnn.pad_sequence(audio_batch, batch_first=True)
+        audio_lens = torch.cat(audio_lens)
+        res = model.decode(audio_batch, audio_lens, ctc=settings.encoder_outputs, verbatim=settings.verbatim_decoder_outputs, subtitle=settings.subtitle_decoder_outputs)
     
-        out = format_output(res, speech_segments, add_timing=output_add_timing, batch=True, verbose=verbose_output)
+        out = format_output(res, speech_segments, add_timing=settings.output_add_timing, batch=True, verbose=settings.verbose_output)
     else:
         # decode segment by segment
         batch_out = []
         for seg in speech_segments:
-            wav_seg = audio_file[int(seg['start']*SR):int(seg['end']*SR)].unsqueeze(0).to(device=device)
-            wav_len = torch.tensor([wav_seg.shape[1],], dtype=torch.long, device=device)
-            res = model.decode(wav_seg, wav_len, ctc=encoder_outputs, verbatim=verbatim_decoder_outputs, subtitle=subtitle_decoder_outputs)
+            audio_seg = audio_file[int(seg['start']*SR):int(seg['end']*SR)].unsqueeze(0).to(device=settings.device)
+            audio_len = torch.tensor([audio_seg.shape[1],], dtype=torch.long, device=settings.device)
+            res = model.decode(audio_seg, audio_len, ctc=settings.encoder_outputs, verbatim=settings.verbatim_decoder_outputs, subtitle=settings.subtitle_decoder_outputs)
             
-            out = format_output(res, [seg], add_timing=output_add_timing, verbose=verbose_output)
+            out = format_output(res, [seg], add_timing=settings.output_add_timing, verbose=settings.verbose_output)
             batch_out.append(out)
         out = merge_batch_outputs(batch_out)
 
